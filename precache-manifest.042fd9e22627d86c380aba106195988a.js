b7e1d32ccabc82c3de1d8c05e9419903bad32f35
ahbashar <basharabuhawila@gmail.com>
2020-06-11 20:47:06 +0300

    Updates

---
+++ precache-manifest.042fd9e22627d86c380aba106195988a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eda08769b12c4dee371ad2f2028e03a4",
+    "url": "/kittens-app/index.html"
+  },
+  {
+    "revision": "69cce648120f2663378a",
+    "url": "/kittens-app/static/css/main.1513b112.chunk.css"
+  },
+  {
+    "revision": "80de6e261c3a1f36230c",
+    "url": "/kittens-app/static/js/2.05463ef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kittens-app/static/js/2.05463ef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69cce648120f2663378a",
+    "url": "/kittens-app/static/js/main.d48fb33e.chunk.js"
+  },
+  {
+    "revision": "cf0008adb40aee1e9ea7",
+    "url": "/kittens-app/static/js/runtime-main.3fcd8d7f.js"
+  }
+]);
